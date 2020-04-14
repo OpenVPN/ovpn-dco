a93829e21d25bc5c65ed036c854514c2f87e2370
Antonio Quartulli <antonio@openvpn.net>
2020-04-14 16:38:10 +0200

    ovpn-dco: tests - update qemu script with SHARE variable
    Signed-off-by: Antonio Quartulli <antonio@openvpn.net>

--- tests/qemu/launch_deb2.sh
+++ tests/qemu/launch_deb2.sh
@@ -2,6 +2,7 @@
 
 DISK=emulation/debian2.img
 BZIMAGE=linux-kernel/arch/x86/boot/bzImage
+SHARE=/home/user/share
 
 sudo qemu-system-x86_64 \
     -device virtio-balloon \
@@ -10,7 +11,7 @@ sudo qemu-system-x86_64 \
     -kernel ${BZIMAGE} \
     -append 'console=ttyS0 root=/dev/vda1 pci=noacpi' \
     -drive file=${DISK},if=virtio,cache=writeback \
-    -virtfs local,path=/home/ordex/exp,security_model=passthrough,mount_tag=k1 \
+    -virtfs local,path=${SHARE},security_model=passthrough,mount_tag=k1 \
     -nic tap,ifname=tap0,script=no,downscript=no,model=virtio-net-pci \
     -display none \
     -nographic \
