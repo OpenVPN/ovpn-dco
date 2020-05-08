e849d0099419c6cb0f7414eaf90b5db62f710388
Antonio Quartulli <antonio@openvpn.net>
2020-05-09 00:31:55 +0200

    ovpn-dco: add NETIF_F_LLTX to the device feature
    Being ovpn-dco a fotware tunnel, it does not require TX locking.
    Concurrent calls are protected by internal rcu/locking.
    
    Signed-off-by: Antonio Quartulli <antonio@openvpn.net>

--- net/ovpn-dco/main.c
+++ net/ovpn-dco/main.c
@@ -169,7 +169,7 @@ static const struct ethtool_ops ovpn_ethtool_ops = {
 
 static void ovpn_setup(struct net_device *dev)
 {
-	netdev_features_t feat = NETIF_F_SG | NETIF_F_FRAGLIST |
+	netdev_features_t feat = NETIF_F_SG | NETIF_F_FRAGLIST | NETIF_F_LLTX |
 				 NETIF_F_HW_CSUM | NETIF_F_RXCSUM;
 
 	dev->ethtool_ops = &ovpn_ethtool_ops;
