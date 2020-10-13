e363b7dbba7972b73c15c396374a99ef88e08e08
Lev Stipakov <lev@openvpn.net>
2020-10-13 11:16:55 +0300

    ovpn-dco: take ip_tunnel_parse_protocol() into use
    Commit 2606aff91685 ("net: ip_tunnel: add header_ops for layer 3 devices")
    has introduced ip_tunnel_parse_protocol() function for general use.
    
    Drop our own implementation (ovpn_ip_get_protocol) in favor of above
    function.
    
    Signed-off-by: Lev Stipakov <lev@openvpn.net>
    Signed-off-by: Antonio Quartulli <antonio@openvpn.net>

--- drivers/net/ovpn-dco/skb.h
+++ drivers/net/ovpn-dco/skb.h
@@ -11,6 +11,7 @@
 #define _NET_OVPN_DCO_SKB_H_
 
 #include <linux/types.h>
+#include <net/ip_tunnels.h>
 
 #define OVPN_SKB_CB(skb) ((struct ovpn_skb_cb *)&((skb)->cb))
 
@@ -29,36 +30,9 @@ static inline u32 ovpn_skb_queue_len(const struct sk_buff_head *list)
 	return READ_ONCE(list->qlen);
 }
 
-/* Probe IP header and do basic sanity checking on
- * IP packet in skb.
- */
-static inline __be16 ovpn_ip_get_protocol(struct sk_buff *skb)
-{
-	const struct iphdr *iph;
-
-	if (unlikely(!pskb_may_pull(skb, sizeof(struct iphdr))))
-		return 0;
-
-	iph = ip_hdr(skb);
-	switch (iph->version) {
-	case 4:
-		/* make sure that IPv4 packet doesn't have a bogus length */
-		if (unlikely((iph->ihl << 2) < sizeof(struct iphdr)))
-			return 0;
-		return htons(ETH_P_IP);
-	case 6:
-		/* for IPv6, check for larger header size */
-		if (unlikely(!pskb_may_pull(skb, sizeof(struct ipv6hdr))))
-			return 0;
-		return htons(ETH_P_IPV6);
-	default:
-		return 0;
-	}
-}
-
 static inline int ovpn_ip_check_protocol(struct sk_buff *skb)
 {
-	__be16 proto = ovpn_ip_get_protocol(skb);
+	__be16 proto = ip_tunnel_parse_protocol(skb);
 
 	if (unlikely(!proto))
 		return -EPROTONOSUPPORT;
@@ -71,7 +45,7 @@ static inline int ovpn_ip_check_protocol(struct sk_buff *skb)
 
 static inline int ovpn_ip_header_probe(struct sk_buff *skb)
 {
-	__be16 proto = ovpn_ip_get_protocol(skb);
+	__be16 proto = ip_tunnel_parse_protocol(skb);
 
 	if (!proto)
 		return -EPROTONOSUPPORT;
