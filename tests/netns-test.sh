8fbe96e2c166ba609899cb3fd82ba110d65f07b8
Antonio Quartulli <antonio@openvpn.net>
2021-01-26 15:12:24 +0100

    ovpn-dco: netns-test.sh - allow creating a simple VPN network
    The testing script has been extended to work with the new
    ovpn-cli syntax and create a basic IPv4 multi-peer network.
    
    IPv6 and TCP cannot yet be tested using this script.
    
    Signed-off-by: Antonio Quartulli <antonio@openvpn.net>

--- tests/netns-test.sh
+++ tests/netns-test.sh
@@ -7,31 +7,47 @@
 set -x
 set -e
 
-OVPN_CLI=./ovpn-cli
+OVPN_CLI=${OVPN_CLI:-./ovpn-cli}
 ALG=${ALG:-aes}
+NUM_PEERS=${NUM_PEERS:-2}
 
 function create_ns() {
-	ip -n peer$1 link del tun0 || true
-	ip -n peer$1 link del veth$1 || true
-	ip netns del peer$1 || true
 	ip netns add peer$1
 }
 
 function setup_ns() {
-	ip link set veth$1 netns peer$1
-	ip -n peer$1 addr add $2/$3 dev veth$1
-	ip -n peer$1 link set veth$1 up
+	if [ $1 -eq 0 ]; then
+		for p in $(seq 1 $NUM_PEERS); do
+			ip link add veth${p} netns peer0 type veth peer name veth${p} netns peer${p}
+
+			ip -n peer0 addr add 10.10.${p}.1/24 dev veth${p}
+			ip -n peer0 link set veth${p} up
+
+			ip -n peer${p} addr add 10.10.${p}.2/24 dev veth${p}
+			ip -n peer${p} link set veth${p} up
+		done
+	fi
+
 	if [ $ipv6 -eq 1 ]; then
 		sleep 5
 	fi
 
 	ip -n peer$1 link add tun0 type ovpn-dco
-	ip -n peer$1 addr add $4 dev tun0
+	ip -n peer$1 addr add $2 dev tun0
 	ip -n peer$1 link set tun0 up
+}
 
+function add_peer() {
 	if [ $tcp -eq 0 ]; then
-		ip netns exec peer$1 $OVPN_CLI tun0 new_peer $5 $6 $7 $8
-		ip netns exec peer$1 $OVPN_CLI tun0 new_key $ALG $1 data64.key
+		if [ $1 -eq 0 ]; then
+			for p in $(seq 1 $NUM_PEERS); do
+				ip netns exec peer0 $OVPN_CLI tun0 new_peer ${p} ${p} 10.10.${p}.2 1 5.5.5.$((${p} + 1))
+				ip netns exec peer0 $OVPN_CLI tun0 new_key ${p} $ALG 0 data64.key
+			done
+		else
+			ip netns exec peer${1} $OVPN_CLI tun0 new_peer ${1} 1 10.10.${1}.1 ${1} 5.5.5.1
+			ip netns exec peer${1} $OVPN_CLI tun0 new_key ${1} $ALG 1 data64.key
+		fi
 	else
 		if [ $1 -eq 0 ]; then
 			(ip netns exec peer$1 $OVPN_CLI tun0 listen $5 $8 $9 && \
@@ -43,11 +59,19 @@ function setup_ns() {
 	fi
 }
 
-create_ns 0
-create_ns 1
 
-ip link del veth0 || true
-ip link add veth0 type veth peer name veth1
+# clean up
+for p in $(seq 1 10); do
+	ip -n peer0 link del veth${p} 2>/dev/null || true
+done
+for p in $(seq 0 10); do
+	ip -n peer${p} link del tun0 2>/dev/null || true
+	ip netns del peer${p} 2>/dev/null || true
+done
+
+for p in $(seq 0 $NUM_PEERS); do
+	create_ns ${p}
+done
 
 ipv6=0
 if [ "$1" == "-6" ]; then
@@ -65,9 +89,17 @@ fi
 if [ $ipv6 -eq 1 ]; then
 	setup_ns 0 fc00::1 64 5.5.5.1/24 1 fc00::2 2 5.5.5.2 ipv6
 	setup_ns 1 fc00::2 64 5.5.5.2/24 2 fc00::1 1 5.5.5.1 ipv6
+	setup_ns 2 fc00::2 64 5.5.5.2/24 2 fc00::1 1 5.5.5.1 ipv6
 else
-	setup_ns 0 10.10.10.1 24 5.5.5.1/24 1 10.10.10.2 2 5.5.5.2
-	setup_ns 1 10.10.10.2 24 5.5.5.2/24 2 10.10.10.1 1 5.5.5.1
+	for p in $(seq 0 $NUM_PEERS); do
+		setup_ns ${p} 5.5.5.$((${p} + 1))/24
+	done
+
+	for p in $(seq 0 $NUM_PEERS); do
+		add_peer ${p}
+	done
 fi
 
-ip netns exec peer0 ping -c 3 5.5.5.2
+for p in $(seq 1 $NUM_PEERS); do
+	ip netns exec peer0 ping -c 1 5.5.5.$((${p} + 1))
+done
