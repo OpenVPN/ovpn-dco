dc2a2dbd56a8e71c799aff1cc3d06f69acedfc4f
Antonio Quartulli <antonio@openvpn.net>
2026-08-03 16:06:03 +0200

    ovpn-dco: add hack for setup_udp_tunnel_sock() on linux >=7.1.5
    Signed-off-by: Antonio Quartulli <antonio@openvpn.net>

--- linux-compat.h
+++ linux-compat.h
@@ -40,6 +40,20 @@
 #define SUSE_PRODUCT(pr, v, pl, aux) 1
 #endif
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(7, 1, 5)
+
+#include <net/udp_tunnel.h>
+
+static inline void ovpn_setup_udp_tunnel_sock(struct net *net, struct sock *sk,
+					      struct udp_tunnel_sock_cfg *cfg)
+{
+	setup_udp_tunnel_sock(net, sk->sk_socket, cfg);
+}
+
+#define setup_udp_tunnel_sock ovpn_setup_udp_tunnel_sock
+
+#endif /* LINUX_VERSION_CODE < KERNEL_VERSION(7, 1, 5) */
+
 #if LINUX_VERSION_CODE < KERNEL_VERSION(7, 1, 0)
 
 #include <net/ipv6_stubs.h>
